882ff242189d79838473be0a7f7728f7b02c9e43
thi777 <thiagocarvalho.ads@gmail.com>
2020-08-10 21:58:07 -0300

    feat(categories) create migrate category

---
+++ src/database/migrations/06_categoties.js
@@ -0,0 +1,28 @@
+exports.up = knex => {
+    return knex.schema.createTable('categories', table => {
+        table
+            .increments('id')
+            .unsigned()
+            .primary()
+            .unique();
+
+        table.string('name').notNullable();
+        table.boolean('isActive').defaultTo(true);
+        table.specificType('products', 'int[]');
+
+        table.integer('store_id');
+        table
+            .foreign('store_id')
+            .references('id')
+            .inTable('stores')
+            .onUpdate('CASCADE')
+            .onDelete('SET NULL');
+
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = knex => {
+    knex.schema.dropTable('categories');
+};
