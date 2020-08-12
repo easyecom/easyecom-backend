d8ff63cf70785e8d27f419af8d54f0fecc696710
thi777 <thiagocarvalho.ads@gmail.com>
2020-08-11 23:51:57 -0300

    feat(brands) create migrate brand

---
+++ src/database/migrations/07_brands.js
@@ -0,0 +1,28 @@
+exports.up = knex => {
+    return knex.schema.createTable('brands', table => {
+        table
+            .increments('id')
+            .unsigned()
+            .primary()
+            .unique();
+
+        table.string('name').notNullable();
+        table.string('description')
+        table.boolean('isActive').defaultTo(true);
+
+        table.integer('store_id')
+        table
+            .foreign('store_id')
+            .references('id')
+            .inTable('stores')
+            .onUpdate('CASCADE')
+            .onDelete('SET NULL');
+
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+exports.down = knex => {
+    knex.schema.dropTable('brands');
+};
