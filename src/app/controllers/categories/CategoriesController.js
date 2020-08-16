5ebdceb57e884e1c8cfeec5667ff24ea41973eda
thi777 <thiagocarvalho.ads@gmail.com>
2020-08-15 22:07:10 -0300

    feat(category): active category flow

--- src/app/controllers/categories/CategoriesController.js
+++ src/app/controllers/categories/CategoriesController.js
@@ -2,15 +2,15 @@ import connection from '../../../database/connection';
 
 class CategoriesController {
     async store(req, res) {
-        const { name, isActive, store_id, products } = req.body;
+        const { store_id } = req.params;
+        const { name, isActive, description } = req.body;
 
         try {
             let error = [];
 
             if (!name) error.push('name');
-            if (!isActive) error.push('isActive');
-            if (!store_id) error.push('store_id');
-            if (!products) error.push('products');
+            if (!store_id) error.push('params store_id');
+            if (!description) error.push('description');
 
             if (error.length > 0) {
                 return res
@@ -24,10 +24,66 @@ class CategoriesController {
                     name,
                     isActive,
                     store_id,
-                    products,
+                    description,
                 });
 
             return res.status(201).json('categories performed successfully');
+        } catch (err) {
+            return res.status(500).json('sorry, something broke...');
+        }
+    }
+
+    async getAll(req, res) {
+        try {
+            const data = await connection('categories')
+                .select('*')
+                .where('isActive', true);
+            return res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json('sorry, something broke...');
+        }
+    }
+
+    async getOne(req, res) {
+        const { category_id } = req.params;
+
+        try {
+            const data = await connection('categories')
+                .select('*')
+                .where('id', category_id);
+            return res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json('sorry, something broke...');
+        }
+    }
+
+    async update(req, res) {
+        const { category_id, store_id } = req.params;
+        const { name, isActive, description } = req.body;
+
+        try {
+            const data = await connection('categories')
+                .where('id', category_id)
+                .update({ name, isActive, store_id, description }, [
+                    'name',
+                    'isActive',
+                    'store_id',
+                    'description',
+                ]);
+            return res.status(200).json(data);
+        } catch (err) {
+            return res.status(500).json('sorry, something broke...');
+        }
+    }
+
+    async delete(req, res) {
+        const { category_id } = req.params;
+
+        try {
+            await connection('categories')
+                .del()
+                .where('id', category_id);
+            return res.status(200).json('category deleted successfully');
         } catch (err) {
             return res.status(500).json('sorry, something broke...');
         }
