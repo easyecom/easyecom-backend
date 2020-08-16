a9890eda8e566bd57d2bbc6d691a2d2b998f17a8
thi777 <thiagocarvalho.ads@gmail.com>
2020-08-15 21:22:11 -0300

    refactor(store validade): changing variable name

--- src/app/controllers/validator/storeValidation.js
+++ src/app/controllers/validator/storeValidation.js
@@ -1,33 +1,39 @@
 const connection = require('../../../database/connection');
 
 export default async (req, res, next) => {
-    const { user_id } = req.headers;
+    const { user_admin } = req.headers;
 
-    if (!user_id) return res.status(401).json('this action needs a user');
+    if (!user_admin) return res.status(401).json('need to be an user admin');
 
-    const { id } = req.params;
+    const { store_id } = req.params;
+
+    if (store_id == 0 || !store_id || !store_id.length) {
+        return res.status(401).json('please, put id store');
+    }
 
     const user = await connection('users')
         .select('*')
-        .where('id', user_id);
+        .where('id', user_admin);
 
     if (!user.length) {
         return res.status(401).json('user does not exist');
     }
 
     if (!user[0].permission.includes('admin')) {
-        return res.status(401).json('user does not have administrator permission');
+        return res
+            .status(401)
+            .json('user does not have administrator permission');
     }
 
     const store = await connection('stores')
         .select('*')
-        .where('id', id);
+        .where('id', store_id);
 
     if (!store.length) {
-        return res.status(401).json('not found');
+        return res.status(401).json('store not found');
     }
 
-    if (user[0].store_id.toString() !== id) {
+    if (user[0].store_id.toString() !== store_id) {
         return res.status(401).json('user does not have permission');
     }
 
