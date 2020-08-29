c00217bb5cbb61426d1daf7a486853b0775f346e
thi777 <thiagocarvalho.ads@gmail.com>
2020-08-29 20:42:52 -0300

    feat(client): finished resources

--- src/app/controllers/clients/ClientController.js
+++ src/app/controllers/clients/ClientController.js
@@ -2,21 +2,22 @@ import connection from '../../../database/connection';
 
 class ClientController {
     async store(req, res) {
+        const { store_id, client_id: user_id } = req.params;
+
+        const { dateOfBirth, cpf } = req.body;
+
+        let error = [];
+
+        if (!dateOfBirth) error.push('dateOfBirth');
+        if (!cpf) error.push('cpf');
+
+        if (error.length > 0) {
+            return res
+                .status(422)
+                .json({ error: 'you forgot', required: error });
+        }
+
         try {
-            const { dateOfBirth, cpf, user_id } = req.body;
-
-            let error = [];
-
-            if (!dateOfBirth) error.push('dateOfBirth');
-            if (!cpf) error.push('cpf');
-            if (!user_id) error.push('user');
-
-            if (error.length > 0) {
-                return res
-                    .status(422)
-                    .json({ error: 'you forgot', required: error });
-            }
-
             const data = await connection('clients')
                 .returning('*')
                 .insert({
@@ -28,6 +29,21 @@ class ClientController {
         } catch (err) {
             console.error(err);
             return res.status(500).json('sorry, something broke...');
+        }
+    }
+
+    async update(req, res) {
+        const { client_id } = req.params;
+        const clients = req.body;
+
+        try {
+            const data = await connection('clients')
+                .where({ id: client_id })
+                .update(clients, ['dateOfBirth', 'cpf']);
+
+            return res.status(200).json(data);
+        } catch (err) {
+            return console.error({ stack: err.stack });
         }
     }
 
