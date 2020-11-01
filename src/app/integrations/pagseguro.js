c80ad7ef7da6e875727ff5a7859ead0d8a79c3c7
thi777 <thiagocarvalho.ads@gmail.com>
2020-11-01 19:35:28 -0300

    refactor(order): payment pagseguro

--- src/app/integrations/pagseguro.js
+++ src/app/integrations/pagseguro.js
@@ -1,52 +1,25 @@
-require("util").inspect.defaultOptions.depth = null;
-const axios = require('axios').default;
-const pagseguroConfig = require('../../config/pagseguro');
+require('util').inspect.defaultOptions.depth = null;
+
 const billetDataTranformed = require('./helper/billet.dt');
-const { baseUrl } = pagseguroConfig;
-
-const _createPaymentBillet = async (senderHash, data) => {
-    const billet = await billetDataTranformed(senderHash, data);
-
-    console.log('teste data');
-    const result = axios
-        .post(`${baseUrl}/charges`, billet, {
-            headers: {
-                // ['Content-Type']: 'application/json',
-                Authorization: '9D3C5D6F23A94740AE28675BDBB2BA67',
-                ['x-api-version']: '4.0',
-            },
-        })
-        .then(({ data }) => {
-            return console.log(data);
-        })
-        .catch(err => console.error(err));
-    return result;
-};
-
-const _createPaymentCreditCard = async (senderHash, data) => {
-    const billet = await billetDataTranformed();
-    return function() {
-        axios
-            .post(`${baseUrl}/charges`, billet, {
-                Authorization: '9D3C5D6F23A94740AE28675BDBB2BA67',
-                ['x-api-version']: '4.0',
-            })
-            .then(response => {
-                console.log(response, 'response');
-            })
-            .catch(err => console.log(err));
-    };
-};
+const creditCardDataTranformed = require('./helper/credit-card.dt');
+const postPayment = require('./helper/repository');
 
 const createPayment = async (senderHash, data) => {
     const teste = 'boleto';
     try {
         if (teste == 'boleto') {
-            console.log('createPayment');
-            return await _createPaymentBillet(senderHash, data);
+            const billetTransformed = await billetDataTranformed(
+                senderHash,
+                data
+            );
+            return await postPayment(billetTransformed);
         }
-        if (data.payment.paymentForm == 'creditCard') {
-            return await _createPaymentCreditCard(senderHash, data);
+        if (teste == 'creditCard') {
+            const creditCardTranformed = await creditCardDataTranformed(
+                senderHash,
+                data
+            );
+            return await postPayment(creditCardTranformed);
         }
         return { error: 'invalid payment form' };
     } catch (err) {
